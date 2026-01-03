0e87a2a86e237b5bde03355c0cc9736f7d5d4d1f
KilenVf <kilenmillot@icloud.com>
2026-01-03 13:28:13 +0100

    video.py modified

--- video.py
+++ video.py
@@ -1,1 +1,9 @@
-import moviepy
+from moviepy import VideoFileClip, TextClip, CompositeVideoClip
+
+
+clip = ( 
+    VideoFileClip("test.mp4")
+    .subclipped(10, 20)
+    .with_volume_scaled(0.8)
+)
+
