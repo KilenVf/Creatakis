7efe8326f81a9b0b8b35dbfe2bd23928c3930ea5
KilenVf <kilenmillot@icloud.com>
2026-01-08 08:39:12 +0100

    libs

--- Medias/video.py
+++ Medias/video.py
@@ -1,9 +1,4 @@
-from moviepy import *
-from tkinter import Tk, filedialog
-from PyQt5.QtWidgets import QApplication, QLineEdit, QWidget, QDialog, QPushButton, QLabel, QVBoxLayout, QHBoxLayout
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QIcon
-import sys
+from libs import*
 
 """ fenêtres """
 
