7f3183dd895b9d5149b4c189e57450f7cb4af0ea
Tarek <areffalag5@gmail.com>
2026-01-18 16:48:46 +0100

    t

--- media_editor.py
+++ media_editor.py
@@ -4,17 +4,16 @@
 
 from moviepy.editor import VideoFileClip, TextClip, CompositeVideoClip
 from config import CODEC, DEFAULT_DURATION, FONT_SIZE
-from main_window import text
 
 text = None
 
-def create_clip(video_path, text):
-    clip = VideoFileClip(video_path).subclipped(0, DEFAULT_DURATION)
+def create_clip(video_path, text=None):
+    clip = VideoFileClip(video_path).subclip(0, DEFAULT_DURATION)
+    
     if text:
-        return CompositeVideoClip([clip,text])
+        return CompositeVideoClip([clip, text])
     else:
         return clip
-
 
 
 def export_video(video, name):
